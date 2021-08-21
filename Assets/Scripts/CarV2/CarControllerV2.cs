7b82d65465541abb8ab40176906c46a44f11e0f8
ivan-suarez <heyitsivan@protonmail.com>
2021-08-21 12:44:38 +0200

    Added automatic gearbox

--- Assets/Scripts/CarV2/CarControllerV2.cs
+++ Assets/Scripts/CarV2/CarControllerV2.cs
@@ -23,7 +23,16 @@ public class CarControllerV2 : MonoBehaviour
     public float totalPower;
     public float wheelsRPM;
 
-    public AnimationCurve engineTorque;
+    public AnimationCurve enginePower;
+
+    public float engineRPM;
+    public float smoothTime = 0.01f;
+    public float[] gears;
+    public int gearNum = 0;
+
+    public float maxRPM, minRPM;
+    public float KPH;
+
 
     void Start()
     {
@@ -41,12 +50,14 @@ public class CarControllerV2 : MonoBehaviour
             for (int i = 0; i < wheels.Length; i++)
             {
                 wheels[i].motorTorque = (torque/4) * inputManager.throttle;
+                //wheels[i].motorTorque = (totalPower / 4);
             }
         }else if (drive == driveType.rearWheelDrive)
         {
             for (int i = 2; i < wheels.Length; i++)
             {
-                wheels[i].motorTorque = (torque / 2) * inputManager.throttle;
+                //wheels[i].motorTorque = (torque / 2) * inputManager.throttle;
+                wheels[i].motorTorque = (totalPower / 2);
             }
         }
         else
@@ -54,6 +65,7 @@ public class CarControllerV2 : MonoBehaviour
             for (int i = 0; i < wheels.Length -2; i++)
             {
                 wheels[i].motorTorque = (torque / 2) * inputManager.throttle;
+                //wheels[i].motorTorque = (totalPower / 2);
             }
         }
         
@@ -62,12 +74,46 @@ public class CarControllerV2 : MonoBehaviour
             wheels[i].steerAngle = maxTurn * inputManager.steer;
             
         }
-
+        Shifter();
+       // KPH = wheels[0].rpm;
+        KPH = (float)((wheels[0].rpm * (2*wheels[0].radius * 3.14))*0.06);
     }
 
     void CalculateEnginePower()
     {
-        
+        wheelRPM();
+        totalPower = enginePower.Evaluate(engineRPM) * (gears[gearNum]) * inputManager.throttle;
+        //totalPower = 600 * inputManager.throttle;
+
+        float velocity = 0.0f;
+
+     /*   if (engineRPM >= maxRPM || flag)
+        {
+            engineRPM = Mathf.SmoothDamp(engineRPM, maxRPM - 500, ref velocity, 0.05f);
+
+            flag = (engineRPM >= maxRPM - 450) ? true : false;
+           
+        }
+        else
+        {
+            engineRPM = Mathf.SmoothDamp(engineRPM, 1000 + (Mathf.Abs(wheelsRPM) * 3.6f * (gears[gearNum])), ref velocity, smoothTime);
+            
+        }
+        if (engineRPM >= maxRPM + 1000) engineRPM = maxRPM + 1000; // clamp at max
+     */
+        engineRPM = Mathf.SmoothDamp(engineRPM, 1000 + (Mathf.Abs(wheelsRPM) * 3.6f* (gears[gearNum])), ref velocity, smoothTime);
+    }
+
+    void wheelRPM()
+    {
+        float sum = 0;
+        int R = 0;
+        for (int i = 0; i < 4; i++)
+        {
+            sum += wheels[i].rpm;
+            R++;
+        }
+        wheelsRPM = (R != 0) ? sum / R : 0;
     }
 
     void AnimateWheels()
@@ -82,5 +128,28 @@ public class CarControllerV2 : MonoBehaviour
             wheelMesh[i].transform.rotation = wheelRotation;
         }
         
+    }
+
+    void Shifter()
+    {
+
+        if(engineRPM > maxRPM && gearNum < gears.Length -1)
+        {
+            gearNum++;
+           
+        }
+        if (engineRPM < minRPM && gearNum > 0)
+        {
+            gearNum--;
+
+        }
+       /* if (Input.GetKeyDown(KeyCode.E))
+        {
+            gearNum++;
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            gearNum--;
+        }*/
     }
 }
